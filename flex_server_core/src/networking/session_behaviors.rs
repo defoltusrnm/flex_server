26b73940052f911bb8b605d5d5a93f20e215278c
defoltusrnm <defoltusrnm@gmail.com>
2025-03-17 23:57:58 +0300

    dawdwada

--- flex_server_core/src/networking/session_behaviors.rs
+++ flex_server_core/src/networking/session_behaviors.rs
@@ -1,16 +1,26 @@
-use flex_net_core::{error_handling::server_errors::ServerError, networking::connections::NetConnection};
+use flex_net_core::{
+    error_handling::server_errors::ServerError, networking::connections::NetConnection,
+};
 
-pub async fn infinite_read<TConnection>(
-    connection: &mut TConnection,
-) -> Result<(), ServerError>
+pub async fn infinite_read<TConnection>(connection: &mut TConnection) -> Result<(), ServerError>
 where
     TConnection: NetConnection,
 {
     loop {
         match connection.read().await {
-            Ok(_frame) => {
-                log::info!("received message")
-            },
+            Ok(frame) => {
+                let msg_res = frame.to_string().and_then(|x| {
+                    Some(x)
+                        .filter(|y| y.len() > 0)
+                        .map(|y| Ok(y))
+                        .unwrap_or_else(|| Err(ServerError::new(format!("empty read"))))
+                });
+
+                match msg_res {
+                    Ok(msg) => log::info!("received messages {msg}"),
+                    Err(err) => return Err(err),
+                }
+            }
             Err(err) => {
                 return Err(err);
             }
